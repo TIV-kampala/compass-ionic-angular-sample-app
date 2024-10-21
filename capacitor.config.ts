832882b40ccfe4471d1bdfe315c78b18e28292ba
kal-J <kharljay15@gmail.com>
2024-10-21 10:38:46 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'compass-ionic-bridge-ra-sample-app',
+  webDir: 'www'
+};
+
+export default config;
